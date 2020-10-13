838eef9330d0be547204c4dba0fe12c207acca31
vissy <xun.zhu@liangyihui.net>
2020-10-13 15:32:07 +0800

    新增首页文章列表

--- testdata/api_data.py
+++ testdata/api_data.py
@@ -6,6 +6,7 @@
 # 首页接口
 api_homepage = {
     'getbannerlist': '/lyhcc/dhCenterBanner/getBannerList',  # 首页banner列表
+    'gistCenterDocuments':'/lyhcc/dh-medical-center/gistCenterDocuments',#首页的科普文章列表
 }
 
 # 发现页接口
