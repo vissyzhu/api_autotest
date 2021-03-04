72087296d3d436c9ebcf23723204e3ce7d7f70d6
vissy <xun.zhu@liangyihui.net>
2021-03-04 11:17:58 +0800

    增加收件人邮箱

--- testdata/email_data.py
+++ testdata/email_data.py
@@ -8,6 +8,6 @@ email_data = {
     "user":"xun.zhu@liangyihui.net",
     "password":"QAtest2019",
     "sender":"xun.zhu@liangyihui.net",
-    "receiver":["xun.zhu@liangyihui.net"],
-    "cc":["1209354095@qq.com"]
+    "receiver":["xun.zhu@liangyihui.net","weiyang.shi@liangyihui.net","qiyin.feng@liangyihui.net","yayuan.xu@liangyihui.net"],
+    "cc":["wu.ma@liangyihui.net","yao.tu@liangyihui.net","kunhong.chen@liangyihui.net","1209354095@qq.com"]
 }
